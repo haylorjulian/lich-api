10b8740854371f1425afa41022b4d35355dda52d
Julian Haylor <haylorjulian@gmail.com>
2021-08-07 10:20:38 +0100

    🔨 configure start script to ts-node

--- services/mockData.ts
+++ services/mockData.ts
@@ -5,21 +5,21 @@ type Instances = Record<string, InstanceI>;
 
 export const instances: Instances = {
   "1": {
-    id: "1",
+    _id: "1",
     name: "DINK",
     ip: uuidv4(),
     service: "https://operavps.com/",
     targetId: uuidv4(),
   },
   "2": {
-    id: "2",
+    _id: "2",
     name: "DONK",
     ip: uuidv4(),
     service: "https://operavps.com/",
     targetId: uuidv4(),
   },
   "3": {
-    id: "3",
+    _id: "3",
     name: "DOINK",
     ip: uuidv4(),
     service: "https://operavps.com/",
